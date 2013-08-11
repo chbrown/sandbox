a5079bad163cc8392a802bbee3bc5a28176fb2e4
Christopher Brown <io@henrian.com>
2013-08-11 00:57:26 -0500

    Listen on all subnets for the udp sample

--- python/udp.py
+++ python/udp.py
@@ -2,7 +2,8 @@
 
 import socket
 
-UDP_IP = '127.0.0.1'
+# UDP_IP = '127.0.0.1'
+UDP_IP = '0.0.0.0'
 UDP_PORT = 5050
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -11,6 +12,7 @@ sock.bind((UDP_IP, UDP_PORT))
 while True:
     data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
     print 'received message:', data
+    print '  from:', addr
 
 
 # sender
