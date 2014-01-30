23da9888a64b199d75f8b245141d1a4cbff4f6d8
Christopher Brown <io@henrian.com>
2014-01-30 14:34:41 -0600

    Oh, just a little matplotlib hate

---
+++ python/matplot.py
@@ -0,0 +1,21 @@
+import IPython
+
+import matplotlib.pyplot as plt
+# import seaborn
+
+print 'Call this script like: python matplotlib.py'
+
+print
+print 'After it drops into the IPython REPL, type something, and then type Ctrl-C'
+print
+print "Nothing happens until you hit linebreak."
+print "What's the point of signalling Ctrl-C if you have to submit it like it's a normal character?"
+print
+
+IPython.embed()
+
+print
+print "Nice, matplotlib. Thanks for fucking up my REPL."
+print
+print "Seaborn pulls the same shit, obviously."
+print
