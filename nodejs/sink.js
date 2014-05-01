c5e47ae0b2dde3d6f9dbdcc7ca8eb7f53764f1df
Christopher Brown <io@henrian.com>
2014-04-30 23:48:50 -0500

    Add sample node.js CORS echo app

---
+++ nodejs/sink.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+/*jslint node: true */
+var path = require('path');
+var http = require('http');
+var child_process = require('child_process');
+var argv = require('optimist')
+  .default({
+    hostname: '127.0.0.1',
+    port: 9009,
+  }).argv;
+
+var post = function(req, res) {
+  req.on('data', function(chunk) {
+    console.log('chunk', chunk.toString());
+  });
+  req.on('end', function() {
+    res.end();
+  });
+};
+
+http.createServer(function(req, res) {
+  console.log('req.method', req.method);
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Args');
+  res.setHeader('Access-Control-Allow-Methods', '*');
+  // res.setHeader('Access-Control-Expose-Headers', '*');
+  // req.getHeader("Access-Control-Request-Headers"));
+
+  if (req.method == 'POST') {
+    post(req, res);
+  }
+  else {
+    res.end();
+  }
+}).listen(argv.port, argv.hostname, function() {
+  console.log('listening on http://%s:%d', argv.hostname, argv.port);
+});
